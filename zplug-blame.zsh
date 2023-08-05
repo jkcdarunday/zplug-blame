498a6f8e163b74ea6e9dadda1ae06a358187fe47
Jan Keith Darunday <jkcdarunday@gmail.com>
2023-08-05 23:19:10 +0800

    Remove code wrapping of exec and from_cache as they have already been run before this plugin is loaded

--- zplug-blame.zsh
+++ zplug-blame.zsh
@@ -1,32 +1,3 @@
-if typeset -f __zplug::core::arguments::exec > /dev/null; then
-    eval original_"$(declare -f __zplug::core::arguments::exec)"
-
-    __zplug::core::arguments::exec() {
-        local start=$(($(date +%s%N)/1000000))
-        echo "Caching plugins..."
-        original___zplug::core::arguments::exec "$@"
-        env=$(($(date +%s%N)/1000000))
-        echo "Cached plugins $@ ($((end - start))ms))"
-    }
-else
-    echo "Zplug function not found!"
-fi
-
-
-if typeset -f __zplug::core::load::from_cache > /dev/null; then
-    eval original_"$(declare -f __zplug::core::load::from_cache)"
-    
-    __zplug::core::load::from_cache() {
-        local start=$(($(date +%s%N)/1000000))
-        echo "Loading cached plugins $@..."
-        original___zplug::core::load::from_cache "$@"
-        local end=$(($(date +%s%N)/1000000))
-        echo "Loaded cached plugins $@ ($((end - start))ms)"
-    }
-else
-    echo "Cache function not found!"
-fi
-
 if typeset -f __zplug::core::load::as_plugin > /dev/null; then
     eval original_"$(declare -f __zplug::core::load::as_plugin)"
     
@@ -35,7 +6,7 @@ if typeset -f __zplug::core::load::as_plugin > /dev/null; then
         local start=$(($(date +%s%N)/1000000))
         original___zplug::core::load::as_plugin "$@"
         local end=$(($(date +%s%N)/1000000))
-        echo "\tLoaded plugin file $plugin_file ($((end - start))ms)"
+        echo "Loaded plugin file $plugin_file ($((end - start))ms)"
     }
 else
     echo "Plugin load function not found!"
