9e2c650b9fdeb75d7d8e4dd0b3e59a0792174cbf
Jan Keith Darunday <jkcdarunday@gmail.com>
2023-08-06 00:40:44 +0800

    Rename functions to prevent conflicts and add way to print total load time

--- zplug-blame.zsh
+++ zplug-blame.zsh
@@ -1,6 +1,8 @@
+[ -z "$ZPLUG_BLAME_START_TIME" ] && export ZPLUG_BLAME_START_TIME=$(date +%s%N)
+
 autoload colors; colors
 
-initialize_zplug_blame() {
+__zplug_blame::init() {
     fn_name="__zplug::core::load::as_plugin"
 
     if typeset -f original_$fn_name > /dev/null; then
@@ -15,12 +17,12 @@ initialize_zplug_blame() {
 
     eval original_"$(declare -f $fn_name)"
     $fn_name() {
-        local start=$(($(date +%s%N)/1000000))
+        local start=$(date +%s%N)
 
         original_$fn_name "$@"
 
-        local end=$(($(date +%s%N)/1000000))
-        local time_taken=$((end - start))
+        local end=$(date +%s%N)
+        local time_taken=$(((end - start)/1000000))
         local fg_color=$fg[green]
         [ $time_taken -gt 100 ] && fg_color=$fg[yellow]
         [ $time_taken -gt 200 ] && fg_color=$fg[red]
@@ -30,5 +32,15 @@ initialize_zplug_blame() {
     }
 }
 
-initialize_zplug_blame
+zplug_blame::print_load_time() {
+    local end=$(date +%s%N)
+    local time_taken=$(((end - ZPLUG_BLAME_START_TIME)/1000000))
+    local fg_color=$fg[green]
+    [ $time_taken -gt 500 ] && fg_color=$fg[yellow]
+    [ $time_taken -gt 1000 ] && fg_color=$fg[red]
+
+    echo "Finished loading in $fg_color$time_taken ms$reset_color"
+}
+
+__zplug_blame::init
 
